9e90ad08f590b6b07f009f7a6bdff7eba777ccca
Arthur Moreira da Silva <arthur.moreira@fbest.org.br>
2026-01-30 12:27:45 -0300

    Primeira versão

---
+++ app.py
@@ -0,0 +1,12 @@
+import requests
+
+
+def conversion(coin_origin, coin_target):
+    response = requests.get(
+        url= f'https://economia.awesomeapi.com.br/last/{coin_origin}-{coin_target}'
+        )
+    if response.status_code == 404:
+        return response.json().get('message')
+    return response.json().get(f'{coin_origin}{coin_target}').get('bid')
+
+print(conversion('BTC', 'BRL'))
